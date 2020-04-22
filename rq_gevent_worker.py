0ab988d6e24b8b3db180e5d41ecbf00c51c905d9
Michael Abrahamsen <mail@michaelabrahamsen.com>
2020-04-22 17:22:59 -0600

    update to work with version >= 1.2.2

--- rq_gevent_worker.py
+++ rq_gevent_worker.py
@@ -4,26 +4,31 @@ from __future__ import (absolute_import, division, print_function,
 
 from gevent import monkey, get_hub
 from gevent.hub import LoopExit
-monkey.patch_all()
+monkey.patch_all() # noqa
 
 import signal
 import gevent
 import gevent.pool
 from rq import Worker
-try: # for rq >= 0.5.0
+try:  # for rq >= 0.5.0
     from rq.job import JobStatus
-except ImportError: # for rq <= 0.4.6 
+except ImportError:  # for rq <= 0.4.6
     from rq.job import Status as JobStatus
 from rq.timeouts import BaseDeathPenalty, JobTimeoutException
-from rq.worker import StopRequested, green, blue
+from rq.worker import StopRequested, green, blue, WorkerStatus
 from rq.exceptions import DequeueTimeout
 from rq.logutils import setup_loghandlers
 from rq.version import VERSION
 
+DEFAULT_LOGGING_DATE_FORMAT = '%H:%M:%S'
+DEFAULT_LOGGING_FORMAT = '%(asctime)s %(message)s'
+
 
 class GeventDeathPenalty(BaseDeathPenalty):
     def setup_death_penalty(self):
-        exception = JobTimeoutException('Gevent Job exceeded maximum timeout value (%d seconds).' % self._timeout)
+        exception = JobTimeoutException(
+            'Gevent Job exceeded maximum timeout value (%d seconds).' %
+            self._timeout)
         self.gevent_timeout = gevent.Timeout(self._timeout, exception)
         self.gevent_timeout.start()
 
@@ -33,12 +38,14 @@ class GeventDeathPenalty(BaseDeathPenalty):
 
 class GeventWorker(Worker):
     death_penalty_class = GeventDeathPenalty
+    DEFAULT_POOL_SIZE = 20
 
     def __init__(self, *args, **kwargs):
-        pool_size = 20
+        pool_size = self.DEFAULT_POOL_SIZE
         if 'pool_size' in kwargs:
             pool_size = kwargs.pop('pool_size')
         self.gevent_pool = gevent.pool.Pool(pool_size)
+        self.gevent_worker = None
         super(GeventWorker, self).__init__(*args, **kwargs)
 
     def register_birth(self):
@@ -57,26 +64,29 @@ class GeventWorker(Worker):
             raise SystemExit()
 
         def request_stop():
-            if not self._stopped:
-                gevent.signal(signal.SIGINT, request_force_stop)
-                gevent.signal(signal.SIGTERM, request_force_stop)
+            if not self._stop_requested:
+                gevent.hub.signal(signal.SIGINT, request_force_stop)
+                gevent.hub.signal(signal.SIGTERM, request_force_stop)
 
                 self.log.warning('Warm shut down requested.')
                 self.log.warning('Stopping after all greenlets are finished. '
                                  'Press Ctrl+C again for a cold shutdown.')
 
-                self._stopped = True
+                self._stop_requested = True
                 self.gevent_pool.join()
+                if self.gevent_worker is not None:
+                    self.gevent_worker.kill(StopRequested)
 
-            raise StopRequested()
-
-        gevent.signal(signal.SIGINT, request_stop)
-        gevent.signal(signal.SIGTERM, request_stop)
+        gevent.hub.signal(signal.SIGINT, request_stop)
+        gevent.hub.signal(signal.SIGTERM, request_stop)
 
     def set_current_job_id(self, job_id, pipeline=None):
         pass
 
-    def work(self, burst=False):
+    def _work(self, burst=False, logging_level="INFO",
+              date_format=DEFAULT_LOGGING_DATE_FORMAT,
+              log_format=DEFAULT_LOGGING_FORMAT,
+              max_jobs=None, with_scheduler=False):
         """Starts the work loop.
 
         Pops and performs all jobs on the current list of queues.  When all
@@ -85,24 +95,33 @@ class GeventWorker(Worker):
 
         The return value indicates whether any jobs were processed.
         """
-        setup_loghandlers()
+        setup_loghandlers(logging_level)
         self._install_signal_handlers()
 
         self.did_perform_work = False
         self.register_birth()
-        self.log.info('RQ worker started, version %s' % VERSION)
-        self.set_state('starting')
+        self.log.info("RQ GEVENT worker (Greenlet pool size={0}) {1!r} started, version {2}".
+                      format(self.gevent_pool.size, self.key, VERSION))
+        self.set_state(WorkerStatus.STARTED)
+
         try:
             while True:
-                if self.stopped:
-                    self.log.info('Stopping on request.')
-                    break
-
-                timeout = None if burst else max(1, self.default_worker_ttl - 60)
                 try:
-                    result = self.dequeue_job_and_maintain_ttl(timeout)
+                    self.check_for_suspension(burst)
 
+                    if self.should_run_maintenance_tasks:
+                        self.clean_registries()
+
+                    if self._stop_requested:
+                        self.log.info('Stopping on request.')
+                        break
+
+                    timeout = None if burst else max(1, self.default_worker_ttl - 60)
+
+                    result = self.dequeue_job_and_maintain_ttl(timeout)
                     if result is None and burst:
+                        self.log.info("RQ worker {0!r} done, quitting".format(self.key))
+
                         try:
                             # Make sure dependented jobs are enqueued.
                             get_hub().switch()
@@ -123,6 +142,22 @@ class GeventWorker(Worker):
                 self.register_death()
         return self.did_perform_work
 
+    def work(self, burst=False, logging_level="INFO",
+             date_format=DEFAULT_LOGGING_DATE_FORMAT,
+             log_format=DEFAULT_LOGGING_FORMAT,
+             max_jobs=None, with_scheduler=False):
+        """
+        Spawning a greenlet to be able to kill it when it's blocked dequeueing job
+        :param burst: if it's burst worker don't need to spawn a greenlet
+        """
+        # If the is a burst worker it's not needed to spawn greenlet
+        if burst:
+            return self._work(burst, logging_level)
+
+        self.gevent_worker = gevent.spawn(self._work, burst)
+        self.gevent_worker.join()
+        return self.gevent_worker.value
+
     def execute_job(self, job, queue):
         def job_done(child):
             self.did_perform_work = True
@@ -130,27 +165,36 @@ class GeventWorker(Worker):
             if job.get_status() == JobStatus.FINISHED:
                 queue.enqueue_dependents(job)
 
-        child_greenlet = self.gevent_pool.spawn(self.perform_job, job)
+        child_greenlet = self.gevent_pool.spawn(self.perform_job, job, queue)
         child_greenlet.link(job_done)
 
     def dequeue_job_and_maintain_ttl(self, timeout):
-        if self._stopped:
+        if self._stop_requested:
             raise StopRequested()
 
         result = None
         while True:
-            if self._stopped:
+            if self._stop_requested:
                 raise StopRequested()
 
             self.heartbeat()
 
+            if self.gevent_pool.full():
+                self.set_state(WorkerStatus.BUSY)
+                self.log.warning(
+                    "RQ GEVENT worker greenlet pool empty current size %s",
+                    self.gevent_pool.size)
+
             while self.gevent_pool.full():
                 gevent.sleep(0.1)
-                if self._stopped:
+                if self._stop_requested:
                     raise StopRequested()
 
             try:
-                result = self.queue_class.dequeue_any(self.queues, timeout, connection=self.connection)
+                result = self.queue_class.dequeue_any(
+                    self.queues, timeout, connection=self.connection
+                )
+                self.set_state(WorkerStatus.IDLE)
                 if result is not None:
                     job, queue = result
                     self.log.info('%s: %s (%s)' % (green(queue.name),
@@ -165,12 +209,12 @@ class GeventWorker(Worker):
 
 def main():
     import sys
-    from rq.scripts.rqworker import main as rq_main
+    from rq.cli import worker as rq_main
 
     if '-w' in sys.argv or '--worker-class' in sys.argv:
         print("You cannot specify worker class when using this script,"
-                "use the official rqworker instead")
+              "use the official rqworker instead")
         sys.exit(1)
 
-    sys.argv.extend(['-w', 'rq_gevent_worker.GeventWorker'])
+    sys.argv.extend(['-w', 'gevent_rqworker.GeventWorker'])
     rq_main()
